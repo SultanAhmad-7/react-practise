1f05c28073c37f44cedbc9eda607e7325e828555
sultan ahmad <sultan.asim04@gmail.com>
2023-08-22 00:22:25 +0500

    app js at netlify -U

--- src/App.js
+++ src/App.js
@@ -4,20 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Welcome to learn react app.
-        </a>
-      </header>
+        <h1>Welcome to learning react</h1>
     </div>
   );
 }
